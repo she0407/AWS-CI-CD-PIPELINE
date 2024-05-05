1b4ed49b778cf2f1f3f191d2ede9361a3276859e
she <166332089+she0407@users.noreply.github.com>
2024-05-05 14:42:25 -0400

    add file for codedeploy

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull she47/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 she47/simple-python-flask-app
